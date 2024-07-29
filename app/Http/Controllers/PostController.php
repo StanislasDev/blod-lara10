f7dad970a5ab10e17e17e8989522f940449d7068
Stanislas <simostanley91@gmail.com>
2024-07-29 10:18:34 +0100

    Fin du Scope début du formulaire

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,38 +6,29 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 class PostController extends Controller
 {
-    public function index()
+    public function index(Request $request): View
     {
-        $posts = Post::paginate(7);
-        $total = Post::count();
-        // $posts = Post::all();
-// return view('posts.index',[
-// 'posts' => $posts,
-// 'total' => $total
-// ]);
-        return view('posts.index', compact(['posts', 'total']));
+        return $this->postsView($request->search ? ['search' => $request->search] : []);
     }
 
     public function postsByCategory(Category $category): View
     {
-        return view('posts.index', [
-            // 'posts' => $category->posts()->latest()->paginate(10)
-            'posts' => Post::where(
-                'category_id', $category->id
-            )->latest()->paginate(10),
-        ]);
+        return $this->postsView(['category' => $category]);
     }
 
     public function postsByTag(Tag $tag): View
     {
+        return $this->postsView(['tag' => $tag]);
+    }
+
+    protected function postsView(array $filters): View
+    {
         return view('posts.index', [
-            // 'posts' => $tag->posts()->latest()->paginate(10)
-            'posts' => Post::whereRelation(
-                'tags', 'tags.id', $tag->id
-            )->latest()->paginate(10),
+            'posts' => Post::filters($filters)->latest()->paginate(10),
         ]);
     }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,11 @@
 <x-layout>
     <div class="space-y-10 md:space-y-16 mb-5">
-        @foreach ($posts as $post)
+        @forelse ($posts as $post)
             {{-- <x-post :post="$post" /> --}}
             <x-post :$post list />
-        @endforeach
+            @empty
+            <p class="text-center text-slate-400">Aucun résultat.</p>
+        @endforelse
         {{ $posts->links() }}
     </div>
 </x-layout>
